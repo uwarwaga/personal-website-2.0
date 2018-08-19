c09477b56dfc783d0506403e324ecffc01588497
Aury Bwashi <abwashi@hawk.iit.edu>
2018-08-19 17:51:32 -0500

    removed portfolio and contact pages

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
-var portfolioRouter = require('./routes/portfolio');
-var contactRouter = require('./routes/contact');
 
 
 var app = express();
@@ -26,8 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
-app.use('/portfolio', portfolioRouter);
-app.use('/contact', contactRouter);
 
 
 // catch 404 and forward to error handler
